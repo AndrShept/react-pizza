0fb3afab2427666d00df89c3f8541d750c1cc8a5
lolokos86 <lolokos1986@gmail.com>
2023-02-17 00:26:45 +0200

    modified:   src/App.js 	renamed:    src/img/arrow-top.svg -> src/assets/img/arrow-top.svg 	renamed:    src/img/cart.svg -> src/assets/img/cart.svg 	renamed:    src/img/empty-cart.png -> src/assets/img/empty-cart.png 	renamed:    src/img/grey-arrow-left.svg -> src/assets/img/grey-arrow-left.svg 	renamed:    src/img/pizza-logo.svg -> src/assets/img/pizza-logo.svg 	renamed:    src/img/plus.svg -> src/assets/img/plus.svg 	renamed:    src/img/trash.svg -> src/assets/img/trash.svg 	new file:   src/assets/pizza.json 	new file:   src/components/Categories.jsx 	new file:   src/components/Header.jsx 	new file:   src/components/PizzaBlock.jsx 	new file:   src/components/Sort.jsx

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const Categories = () => {
+  return (
+    <div className='categories'>
+      <ul>
+        <li className='active'>Всі</li>
+        <li>М'ясна</li>
+        <li>Вегерянська</li>
+        <li>Гриль</li>
+        <li>Гостра</li>
+        <li>Закрита</li>
+      </ul>
+    </div>
+  );
+};
